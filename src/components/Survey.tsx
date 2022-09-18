0c388b06364731ca3be96fe4b2d86e7ed35468e0
jsm8109jsm <jsm8109jsm@gmail.com>
2022-09-18 22:35:49 +0900

    ADD :: survey radio

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -15,11 +15,39 @@ interface stat {
 }
 
 function Survey() {
-  const [stat, setStat] = useState<stat[]>([]);
-  
+  const [stat, setStat] = useState<stat>();
+  const statArray = [
+    "유머",
+    "인성",
+    "전문성",
+    "공평성",
+    "간식 준비성",
+    "겸손",
+    "열정",
+    "고집",
+    "권위주의",
+    "급발진력",
+  ];
   return (
     <div>
       <Header />
+      {statArray.map((item, index) => {
+        return (
+          <div key={index}>
+            <span>
+              {item} :
+              {[...Array(10)].map((item, index) => {
+                return (
+                  <span key={index}>
+                    <input type="radio" name="humor" />
+                    <label htmlFor="humor">{index + 1}</label>
+                  </span>
+                );
+              })}
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/teacherInfo.js
+++ src/components/teacherInfo.js
@@ -1,64 +1,75 @@
 import classNames from "classnames";
-import { Header } from '../allFiles';
+import { Header } from "../allFiles";
 import "../styles/teacherInfo.scss";
 import { AiOutlineCaretDown } from "react-icons/ai";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { useState } from 'react';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import { useState } from "react";
 
-function TeacherInfo()
-{
-    const postCommnet = () => {
-        
-    }
+function TeacherInfo() {
+  const postCommnet = () => { };
 
-    const [write, setWrite] = useState();
+  const [write, setWrite] = useState();
 
-    return (
-        <div className={classNames("teacher")}>
-            <Header/>
-            <div className={classNames("teacher teacherInfo")}>
-                <button className={classNames("teacherInfo button")}>평가</button>
-                <img src="/images/honggildong.jpg" alt="icon" className={classNames("teacherInfo picture")} />
-                <div className={classNames("teacherInfo ability")}></div>
-                <div className={classNames("teacherInfo explain")}>
-                    <p className={classNames("teacher explain name")}>홍길동</p>
-                    <p className={classNames("teacher explain intro")}>의적이다</p>
-                </div>
-            </div>
-            <div className={classNames("teacher comments")}>
-                <div className={classNames("comments comment")}>
-                    <div className={classNames("comment user")}>
-                        <img src="/images/profileTest.png" alt="icon" className={classNames("user profile")} />
-                        <span className={classNames("user nickname")}>ㅇㅇ</span>
-                    </div>
-                    <p className={classNames("comment content")}>하이</p>
-                    <div className={classNames("comment detail")}>
-                        <AiOutlineCaretDown/>
-                        <button className={classNames("detail button")}>더보기</button>
-                    </div>
-                </div>
-                <div className={classNames("comments comment")}>
-                    <div className={classNames("comment user")}>
-                        <img src="/images/profileTest.png" alt="icon" className={classNames("user profile")} />
-                        <span className={classNames("user nickname")}>ㅇㅇ</span>
-                    </div>
-                    <p className={classNames("comment content")}>ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</p>
-                </div>
-            </div>
-            <div className="teacher write">
-                <CKEditor 
-                    editor={ClassicEditor}
-                    date={write}
-                    onChange={(event, editor)=>{
-                        const data = editor.getData();
-                        setWrite(data);
-                    }}
-                />
-                <button className={classNames("write button")}>작성</button>
-            </div>
+  return (
+    <div className={classNames("teacher")}>
+      <Header />
+      <div className={classNames("teacher teacherInfo")}>
+        <button className={classNames("teacherInfo button")}>평가</button>
+        <img
+          src="/images/honggildong.jpg"
+          alt="icon"
+          className={classNames("teacherInfo picture")}
+        />
+        <div className={classNames("teacherInfo ability")}></div>
+        <div className={classNames("teacherInfo explain")}>
+          <p className={classNames("teacher explain name")}>홍길동</p>
+          <p className={classNames("teacher explain intro")}>의적이다</p>
         </div>
-    )
+      </div>
+      <div className={classNames("teacher comments")}>
+        <div className={classNames("comments comment")}>
+          <div className={classNames("comment user")}>
+            <img
+              src="/images/profileTest.png"
+              alt="icon"
+              className={classNames("user profile")}
+            />
+            <span className={classNames("user nickname")}>ㅇㅇ</span>
+          </div>
+          <p className={classNames("comment content")}>하이</p>
+          <div className={classNames("comment detail")}>
+            <AiOutlineCaretDown />
+            <button className={classNames("detail button")}>더보기</button>
+          </div>
+        </div>
+        <div className={classNames("comments comment")}>
+          <div className={classNames("comment user")}>
+            <img
+              src="/images/profileTest.png"
+              alt="icon"
+              className={classNames("user profile")}
+            />
+            <span className={classNames("user nickname")}>ㅇㅇ</span>
+          </div>
+          <p className={classNames("comment content")}>
+            ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+          </p>
+        </div>
+      </div>
+      <div className="teacher write">
+        <CKEditor
+          editor={ClassicEditor}
+          date={write}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setWrite(data);
+          }}
+        />
+        <button className={classNames("write button")}>작성</button>
+      </div>
+    </div>
+  );
 }
 
 export default TeacherInfo;
