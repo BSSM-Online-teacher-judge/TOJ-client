e56acde17a84ba69d33599f82f092f409994fd3d
jsm8109jsm <jsm8109jsm@gmail.com>
2022-09-19 21:20:59 +0900

    ADD :: survey request

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -1,53 +1,82 @@
 import React, { useState } from "react";
+import { instance } from "../instance";
 import Header from "./Header";
 
-interface stat {
-  humor: number;
-  tenacity: number;
-  expertise: number;
-  fairness: number;
-  snackReadiness: number;
-  modesty: number;
-  passion: number;
-  stubborn: number;
-  authoritarianism: number;
-  SUA: number;
-}
-
 function Survey() {
-  const [stat, setStat] = useState<stat>();
+  const [stat, setStat] = useState<number[]>([]);
   const statArray = [
     "유머",
     "인성",
     "전문성",
     "공평성",
-    "간식 준비성",
+    // "간식 준비성",
     "겸손",
     "열정",
     "고집",
     "권위주의",
     "급발진력",
   ];
+  const submit = async () => {
+    try {
+      const response = await instance.post(
+        "teacher",
+        {
+          humor: stat[0],
+          tenacity: stat[1],
+          expertise: stat[2],
+          fairness: stat[3],
+          // snackReadiness: stat[0],
+          modesty: stat[4],
+          passion: stat[5],
+          stubborn: stat[6],
+          authoritarianism: stat[7],
+          sua: stat[8],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const changeStat = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    let newArray = [...stat];
+    newArray[index] = Number(e.target.value);
+    console.log(newArray);
+    setStat(newArray);
+  };
   return (
     <div>
       <Header />
-      {statArray.map((item, index) => {
+      {statArray.map((statItem, statIndex) => {
         return (
-          <div key={index}>
-            <span>
-              {item} :
+          <div key={statIndex}>
+            <ul>
+              {statItem} :
               {[...Array(10)].map((item, index) => {
                 return (
-                  <span key={index}>
-                    <input type="radio" name="humor" />
-                    <label htmlFor="humor">{index + 1}</label>
-                  </span>
+                  <li key={index}>
+                    <input
+                      type="radio"
+                      name={statItem}
+                      value={index + 1}
+                      onChange={(e) => changeStat(e, statIndex)}
+                    />
+                    <label htmlFor={statItem}>{index + 1}</label>
+                  </li>
                 );
               })}
-            </span>
+            </ul>
           </div>
         );
       })}
+      <button onClick={() => submit()}>제출</button>
     </div>
   );
 }
