633c70598ae647e6401c65b1ab06f537f5e0939c
jsm8109jsm <jsm8109jsm@gmail.com>
2022-10-16 20:01:26 +0900

    FEAT :: AD

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -17,24 +17,10 @@ interface state {
   teacher: teacher;
 }
 
-function SurveyInput() {
-  const location = useLocation().state as state;
-  const { teacher } = location;
+function SurveyInput({ statArray }: { statArray: string[] }) {
   const [stat, setStat] = useState<number[]>([]);
+
   const param = useParams();
-  const statArray = [
-    "유머",
-    "인성",
-    "전문성",
-    "공평성",
-    // "간식 준비성",
-    "겸손",
-    "열정",
-    "고집",
-    "권위주의",
-    "급발진력",
-  ];
-  const [presentStat, setPresentStat] = useState<number[]>([]);
   const submit = async () => {
     try {
       const response = await instance.post(
@@ -70,6 +56,68 @@ function SurveyInput() {
     console.log(newArray);
     setStat(newArray);
   };
+  return (
+    <>
+      <div className={classNames("survey")}>
+        <div className={classNames("table")}>
+          <div className={classNames("survey stat")}>
+            <div className={classNames("survey top")}>
+              {[...Array(10)].map((item, index) => {
+                return (
+                  <span className={classNames("survey th")}>{index + 1}</span>
+                );
+              })}
+            </div>
+          </div>
+          {statArray.map((statItem, statIndex: number) => {
+            return (
+              <ul className={classNames("survey stat")} key={statIndex}>
+                <span className={classNames("survey key")}>{statItem}</span>
+                {[...Array(10)].map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <input
+                        type="radio"
+                        name={statItem}
+                        value={index + 1}
+                        onChange={(e) => changeStat(e, statIndex)}
+                      />
+                      {/* <label htmlFor={statItem}>{index + 1}</label> */}
+                    </li>
+                  );
+                })}
+              </ul>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="submit-div">
+        <button onClick={() => submit()} className="submit">
+          제출
+        </button>
+      </div>
+    </>
+  );
+}
+
+function Survey() {
+  const location = useLocation().state as state;
+  const { teacher } = location;
+  const param = useParams();
+  const statArray = [
+    "유머",
+    "인성",
+    "전문성",
+    "공평성",
+    // "간식 준비성",
+    "겸손",
+    "열정",
+    "고집",
+    "권위주의",
+    "급발진력",
+  ];
+  const [presentStat, setPresentStat] = useState<number[]>([]);
   useEffect(() => {
     (async () => {
       try {
@@ -97,90 +145,39 @@ function SurveyInput() {
       }
     })();
   }, []);
-
-  return (
-    <>
-      <div className={classNames("survey")}>
-        <div className={classNames("title")}>
-          <span className={classNames("name")}>{teacher.name}</span>
-          {teacher.completed ? (
-            <span className={classNames("third complete")}>성공</span>
-          ) : (
-            <span className={classNames("third incomplete")}>실패</span>
-          )}
-        </div>
-        <div className={classNames("table")}>
-          <div className={classNames("topStat")}>
-            {statArray.map((item) => {
-              return (
-                <span className={classNames("topStatItem th")}>{item}</span>
-              );
-            })}
-          </div>
-          <div className={classNames("topStat")}>
-            {presentStat.map((item) => {
-              return (
-                <span className={classNames("topStatItem td")}>{item}</span>
-              );
-            })}
-          </div>
-        </div>
-        <div className={classNames("content")}>
-          <div className={classNames("headline")}>
-            <h3>문제</h3>
-          </div>
-          <p>{teacher.description}</p>
-          <div className={classNames("headline")}>
-            <h3>입력</h3>
-          </div>
-          <div className={classNames("table")}>
-            <div className={classNames("survey stat")}>
-              <div className={classNames("survey top")}>
-                {[...Array(10)].map((item, index) => {
-                  return (
-                    <span className={classNames("survey th")}>{index + 1}</span>
-                  );
-                })}
-              </div>
-            </div>
-            {statArray.map((statItem, statIndex: number) => {
-              return (
-                <ul className={classNames("survey stat")} key={statIndex}>
-                  <span className={classNames("survey key")}>{statItem}</span>
-                  {[...Array(10)].map((item, index) => {
-                    return (
-                      <li key={index}>
-                        <input
-                          type="radio"
-                          name={statItem}
-                          value={index + 1}
-                          onChange={(e) => changeStat(e, statIndex)}
-                        />
-                        {/* <label htmlFor={statItem}>{index + 1}</label> */}
-                      </li>
-                    );
-                  })}
-                </ul>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="submit-div">
-          <button onClick={() => submit()} className="submit">
-            제출
-          </button>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function Survey() {
   return (
     <div>
       <Header />
-      <SurveyInput />
+      <div className={classNames("title")}>
+        <span className={classNames("name")}>{teacher.name}</span>
+        {teacher.completed ? (
+          <span className={classNames("third complete")}>성공</span>
+        ) : (
+          <span className={classNames("third incomplete")}>실패</span>
+        )}
+      </div>
+      <div className={classNames("table")}>
+        <div className={classNames("topStat")}>
+          {statArray.map((item) => {
+            return <span className={classNames("topStatItem th")}>{item}</span>;
+          })}
+        </div>
+        <div className={classNames("topStat")}>
+          {presentStat.map((item) => {
+            return <span className={classNames("topStatItem td")}>{item}</span>;
+          })}
+        </div>
+      </div>
+      <div className={classNames("content")}>
+        <div className={classNames("headline")}>
+          <h3>문제</h3>
+        </div>
+        <p>{teacher.description}</p>
+        <div className={classNames("headline")}>
+          <h3>입력</h3>
+        </div>
+      </div>
+      <SurveyInput statArray={statArray} />
     </div>
   );
 }
---
+++ src/interfaces/ad.tsx
@@ -0,0 +1,7 @@
+export interface ad {
+  id: number;
+  img: string;
+  advertiser: string;
+  link: string;
+  status: "OPEN" | "CLOSE";
+}
